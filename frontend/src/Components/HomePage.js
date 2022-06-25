a297d0f7306ffbb76c1fd3d096cd7808fe38600f
EmmS21 <44911198+EmmS21@users.noreply.github.com>
2022-06-25 10:58:41 -0500

    created navbar

---
+++ frontend/src/Components/HomePage.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import NavBar from '../Sections/HomePage/NavBar';
+
+const HomePage = () => {
+    // const [ auth, setAuth ] = useState(false)
+
+    return (
+        <NavBar/>
+    )
+
+}
+
+export default HomePage;
---
+++ frontend/src/Sections/HomePage/NavBar.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import '../../Assets/CSS/index.css';
+
+
+const NavBar = () => {
+    const [ auth, setAuth ] = useState(false)
+    return (
+        <>
+          <header className='navbar'>
+              <div>
+                  Home
+              </div>
+              <div>
+                  Dashboard
+              </div>
+              <div>
+                  <button>Login</button>
+                  <button>Signup</button>
+              </div>
+          </header>  
+        </>
+    )
+}
+
+export default NavBar;
