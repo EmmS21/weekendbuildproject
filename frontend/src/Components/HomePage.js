e2cfa5d683824298edc6ce8f2f2aefe79d177fbf
Nikhat Elias <nikhatelias@Nikhats-MacBook-Air.local>
2022-07-08 17:44:34 -0700

    Part of the login logic

--- frontend/src/Components/HomePage.js
+++ frontend/src/Components/HomePage.js
@@ -1,20 +1,39 @@
-import React, { useState } from 'react';
-import Hero from '../Sections/HomePage/Hero';
-import NavBar from '../Sections/HomePage/NavBar';
-import Footer from '../Sections/HomePage/Footer';
-
+import React, { useState } from "react";
+import Hero from "../Sections/HomePage/Hero";
+import NavBar from "../Sections/HomePage/NavBar";
+import Footer from "../Sections/HomePage/Footer";
+import { Button, Modal } from "antd";
 
 const HomePage = () => {
-    // const [ auth, setAuth ] = useState(false)
+  const [visible, setVisible] = useState(false);
+  const [confirmLoading, setConfirmLoading] = useState(false);
+  const [modalText, setModalText] = useState("Content of the modal");
+  // const [ auth, setAuth ] = useState(false)
+  const showModal = () => {
+    console.log (visible);
+    setVisible(true);
+  };
 
-    return (
-        <>
-            <NavBar/>
-            <Hero />
-            <Footer />
-        </>
-    )
+  const handleOk = () => {
+    setModalText("The modal will be closed after two seconds");
+    setConfirmLoading(true);
+    setTimeout(() => {
+      setVisible(false);
+      setConfirmLoading(false);
+    }, 2000);
+  };
 
-}
+  const handleCancel = () => {
+    console.log("Clicked cancel button");
+    setVisible(false);
+  };
+  return (
+    <>
+      <NavBar />
+     <Hero />
+      <Footer />
+    </>
+  );
+};
 
 export default HomePage;
--- frontend/src/Sections/HomePage/NavBar.js
+++ frontend/src/Sections/HomePage/NavBar.js
@@ -1,25 +1,73 @@
-import React, { useState } from 'react';
-import '../../Assets/CSS/index.css';
-
+import React, { useState } from "react";
+import "../../Assets/CSS/index.css";
+import { Button, Modal } from "antd";
+import "antd/dist/antd.css";
 
 const NavBar = () => {
-    const [ auth, setAuth ] = useState(false)
-    return (
-        <>
-          <header className='navbar'>
-              <div>
-                  Home
-              </div>
-              <div>
-                  Dashboard
-              </div>
-              <div className='button-group'>
-                  <button>Login</button>
-                  <button>Signup</button>
-              </div>
-          </header>  
-        </>
-    )
-}
+  const [auth, setAuth] = useState(false);
+  const [buttonClicked, setButtonClicked] = useState("");
+  const [visible, setVisible] = useState(false);
+
+    const handleSignUp = () => {
+    setButtonClicked("Signup")}
+    //setVisible(true)
+  
+    const handleLogin = () => {
+        setButtonClicked("Login")}
+    //setVisible(true)
+    
+  
+  return (
+    <>
+      <header className="navbar">
+        <div>Home</div>
+        <div>Dashboard</div>
+
+        
+        <div className="button-group">
+          <Button type="primary" onClick={handleSignUp}>
+            Signup
+          </Button>
+          <Button type="primary" onClick={handleLogin}>
+            Login
+          </Button>
+        </div>
+      </header>
+      {buttonClicked.length > 0 && buttonClicked === "Signup" ?
+        <Modal
+         
+            title='Signup'
+            visible={visible}
+            
+            >
+                <form>
+                    <input id = "email" type = "email" name = "email">
+                        Email
+                    </input>
+                    <input id = "password" type = "password" name = "password">
+                        Password
+                    </input>
+                    <input id = "confirm_password" type = "confirm_password" name = "confirm_password">
+                        Confirm Password
+                    </input>
+                </form>
+        </Modal>
+        : <Modal
+         
+        title='Login'
+        visible={visible}
+        >
+            <form>
+                <input id = "email" type = "email" name = "email">
+                    Email
+                </input>
+                <input id = "password" type = "password" name = "password">
+                    Password
+                </input>
+            </form>
+    </Modal> }
+ </>
+  );
+};
 
 export default NavBar;
